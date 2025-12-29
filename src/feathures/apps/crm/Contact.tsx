0c8785f0e6d5770a62537f3b9ca672f9b4c2cbf8
abhishekbhattarai0 <abhishekbhattarai831@gmail.com>
2025-12-29 14:35:18 +0545

    add contact component in dashboard

--- src/feathures/apps/crm/Contact.tsx
+++ src/feathures/apps/crm/Contact.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 import ContactTable from './components/ContactTable'
 
 const Contact = () => {
-    return (
-        <div className='p-4'>
-            <DashboardWidgetContainer title='Contacts'>
-            <ContactTable/>
-        </DashboardWidgetContainer>
-        </div>
-    )
+  return (
+    <div className='p-4'>
+      <DashboardWidgetContainer title='Contacts'>
+        <ContactTable />
+      </DashboardWidgetContainer>
+    </div>
+  )
 }
 
 export default Contact
--- src/feathures/dashboard/analytics/Analytics.tsx
+++ src/feathures/dashboard/analytics/Analytics.tsx
@@ -3,12 +3,11 @@ import InfoCard from "../components/InfoCard";
 import { analayticsCardInfo } from "@/utils/constants";
 
 import DashboardMultilineChartWidget from "../components/DashboardMultilineChartWidget";
-import DashboardMultibarChartWidget from "../components/DashboardMultibarChartWidget";
-import ProcressPie from "@/components/ui/CircularProgress";
 import DashboardProgressWidget from "../components/DashboardProgressWidget";
-import { PieChart } from "recharts";
 import Card from "../components/Card";
 import DashboardActivityWidget from "../components/DashboardActivityWidget";
+import DashboardWidgetContainer from "@/container/DashboardWidgetContainer";
+import ContactTable from "@/feathures/customers/components/table-page";
 
 const cardData = [
   {
@@ -114,45 +113,14 @@ const Analytics = () => {
             ))}
           </div>
         </div>
+
+
       </div>
+      <DashboardWidgetContainer title='Contacts'>
+        <ContactTable />
+      </DashboardWidgetContainer>
     </div>
   );
 };
 
-// const Analytics = () => {
-//   return (
-//     <div className="mt-2 px-2 space-y-4">
-
-//       {/* Header */}
-//       <div className="flex justify-between px-4 py-3">
-//         <h1 className="text-xl font-bold text-gray-800">Analytics</h1>
-//         <Breadcrumbs />
-//       </div>
-
-//       {/* Analytics Cards */}
-//       <div className="flex gap-4">
-//         <div className="w-3/4 grid grid-cols-1 sm:grid-cols-4 gap-4">
-//           {analayticsCardInfo.map((item, idx) => (
-//             <InfoCard {...item} key={idx} />
-//           ))}
-//         </div>
-
-//         <div className="flex-1">
-//           <DashboardProgressWidget />
-//         </div>
-//       </div>
-
-//       {/* Leads & Vendor */}
-//       <DashboardMultilineChartWidget />
-
-//       {/* Audience Overview */}
-//       <div className="bg-amber-400 p-2">
-//         <DashboardMultibarChartWidget />
-//       </div>
-
-//       {/* Progress */}
-//       <DashboardProgressWidget />
-//     </div>
-//   );
-// };
 export default Analytics;
